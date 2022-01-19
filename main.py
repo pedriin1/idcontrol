f2e708514182108137d36faa1f34240c42f65e95
pedriin1 <frigopedro@icloud.com>
2022-01-19 00:23:49 -0300

    add user done

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get("https://10.211.55.3:30443/#/login")
 
-time.sleep(1)
 typewrite("thisisunsafe")
 
 
@@ -33,55 +32,58 @@ def logIn():
                 time.sleep(1)
 
 
-                driver.get("https://10.211.55.3:30443/#/list_visitor")
 
                 break
             except:
                 pass
 
 
-def addUser(qnt_user):
+def addUser():
+    driver.get("https://10.211.55.3:30443/#/list_visitor")
+    time.sleep(1)
+
+
     select = Select(driver.find_element_by_name('datatablevisitor_length'))
     select.select_by_value('10000')
     driver.find_element_by_xpath("//thead/tr/th[2]").click()
     time.sleep(1)
-    get_lastuser = driver.find_element_by_xpath("//tbody[1]/tr[1]/td[2]") 
+    get_lastuser = driver.find_element_by_xpath("//tbody[1]/tr[1]/td[2]")
+
+    last_user_number = get_lastuser.text
 
     #Criando novo usuario
 
     btn_new = driver.find_element_by_id("btn_add_visitor").click()
     time.sleep(2)
 
-    if get_lastuser.text:
+    if last_user_number:
 
 
-        typewrite(get_lastuser.text)
+        typewrite(str(int(last_user_number) + 1))
     else:
         typewrite("1000")
 
 
     driver.find_element_by_id("visitor_datelimit").clear()
-
     date_picker = driver.find_element_by_id("visitor_datelimit")
-
     date_picker.send_keys("18/01/2026")
-    
+    time.sleep(1)
+    qr_code = driver.find_element_by_xpath("//li[@id='additional']//a")
 
-    print(date_picker)
+    driver.execute_script("arguments[0].click();", qr_code)
 
+    generate_qr_code = driver.find_element_by_id("btn_create_qrcode")
+    driver.execute_script("arguments[0].click();", generate_qr_code)
 
-
-
-    print(get_lastuser.text)
-
+    save = driver.find_element_by_id("btn_save")
+    driver.execute_script("arguments[0].click();", save)
 
 
 def main():
-
-
     logIn()
-    time.sleep(1)
-    addUser(10)
+    for i in range(10):
+        addUser()
+        time.sleep(1)
 
 
 
