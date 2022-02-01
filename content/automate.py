b45106c8c159c6b6f0c1be5a46e794fbee69298c
pedriin1 <frigopedro@icloud.com>
2022-01-31 22:51:31 -0300

    interface connected with automate

---
+++ content/automate.py
@@ -0,0 +1,154 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+from pyautogui import typewrite
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+import json
+
+import os
+
+
+current_dir = os.getcwd()
+
+f = open(f'{current_dir}/content/config.json')
+config = json.load(f)
+f.close()
+
+
+
+LINK = config["ip_address"]
+
+LIST_ADDED_USER = []
+
+
+def logIn(driver):
+    if "login" in driver.current_url:
+        while True:
+            try:
+                user = driver.find_element_by_id("usr")
+                pas = driver.find_element_by_id("pwd")
+
+
+                user.send_keys(config["user"])
+                pas.send_keys(config["password"])
+
+
+                driver.find_element_by_id("entrar").click()
+                time.sleep(1)
+
+
+
+                break
+            except:
+                pass
+
+
+
+
+
+def addUser(last_user, driver):
+
+    global LIST_ADDED_USER
+    #Entrando na pagina para criar o user
+
+    driver.get(f"{LINK}/list_visitor")
+    time.sleep(1)
+    
+    #Criando novo usuario
+
+    btn_new = driver.find_element_by_id("btn_add_visitor").click()
+    time.sleep(2)
+
+
+    #Digitando o nome da usuario
+    name_input = driver.find_element_by_xpath("//div[@class='row form-group']//input[@id='name']")
+    name_input.send_keys(last_user)
+
+    #Digitando a data de expiração
+    driver.find_element_by_id("visitor_datelimit").clear()
+    date_picker = driver.find_element_by_id("visitor_datelimit")
+    date_picker.send_keys("18/01/2026")
+    time.sleep(1)
+
+    #Informaçoes adicionais (adicionar credito)
+
+
+    additional_info = driver.find_element_by_xpath("//li[@id='additional'][2]//a")
+    driver.execute_script("arguments[0].click();", additional_info)
+    add_info_btn = driver.find_element_by_xpath("//div[@class='portlet-title']//div[@class='actions']//a")
+    driver.execute_script("arguments[0].click();", add_info_btn)
+
+
+    time.sleep(1)
+    select = Select(driver.find_element_by_xpath("//div[@class='modal-content']//select"))
+    time.sleep(1)
+
+    select.select_by_value("number:1")
+    time.sleep(1)
+
+    driver.find_element_by_xpath("//div[@class='modal-content']//button[@id='btn_save']").click()
+
+
+
+    # Indo até a pagina de QR Code e gerando
+    qr_code = driver.find_element_by_xpath("//li[@id='additional']//a")
+    driver.execute_script("arguments[0].click();", qr_code)
+
+    generate_qr_code = driver.find_element_by_id("btn_create_qrcode")
+    driver.execute_script("arguments[0].click();", generate_qr_code)
+
+
+    print_qrcode = driver.find_element_by_id("btn_printqrcode")
+    driver.execute_script("arguments[0].click();", print_qrcode)
+
+
+
+
+    time.sleep(2)
+
+    #Salvando o usuario
+    save = driver.find_element_by_id("btn_save")
+    driver.execute_script("arguments[0].click();", save)
+
+    LIST_ADDED_USER.append(last_user)
+
+    time.sleep(1)
+
+
+
+
+def addUserContainer(times, start, driver):
+   
+    for i in range(times):
+        try:
+            addUser(str(start + i), driver)
+        except:
+            time.sleep(5)
+            addUser(str(start + i), driver)
+        time.sleep(1)
+
+
+
+def initialize(qnt_docs, start):
+
+
+
+    
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(f"{LINK}/login")
+    typewrite("thisisunsafe")
+
+    logIn(driver)
+    t1 = time.time()
+    
+    addUserContainer(int(qnt_docs), int(start), driver=driver)
+    t2 = time.time()
+    print(t2-t1)
+
+
+
+
+
--- content/main.py
+++ content/main.py
@@ -1,161 +1,55 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+import tkinter as tk
 
-from pyautogui import typewrite
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+from tkinter import font
+from automate import initialize
 
-import json
 
-import os
+window = tk.Tk()
+window.geometry("300x400")
 
-current_dir = os.getcwd()
+title_font = font.Font(family="Helvetica", size=20, weight=font.BOLD,
+                         )
+title = tk.Label(window, text="Criar usuário", 
+                         font=title_font, width="40", anchor="w", height="2")
 
 
+qnt_placeholder = tk.Label(window, text="Digite a quantidade de usuário", 
+                          width="40", anchor="w")
+qnt_entry = tk.Entry(window, width="40", justify="left")
 
-f = open(f'{current_dir}/content/config.json')
-config = json.load(f)
-f.close()
 
+starter_placeholder = tk.Label(window, text="Digite o número do usuário inicial", 
+                          width="40", anchor="w")
+starter_entry = tk.Entry(window, width="40", justify="left")
 
 
-while True:
-    try:
-        qnt_docs = int(input("Digite a quantidade de docs: "))
-        break
-    except:
-        pass
+start_btn = tk.Button(window, text="Iniciar", width="40")
 
-LINK = config["ip_address"]
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(f"{LINK}/login")
-typewrite("thisisunsafe")
+imprimir = tk.Button(window, text="Imprimir", width="40")
 
 
+status_text = tk.Label(window, text="status", 
+                          width="40", anchor="w")
 
-LIST_ADDED_USER = []
+title.pack(padx=10)
+qnt_placeholder.pack()
+qnt_entry.pack()
 
+starter_placeholder.pack()
+starter_entry.pack()
 
-def logIn():
-    if "login" in driver.current_url:
-        while True:
-            try:
-                user = driver.find_element_by_id("usr")
-                pas = driver.find_element_by_id("pwd")
+start_btn.pack(padx=10, pady=20)
+imprimir.pack(padx=10)
+status_text.pack()
 
 
-                user.send_keys(config["user"])
-                pas.send_keys(config["password"])
+def handle_click(event):
+    print(qnt_entry.get())
 
+    initialize(qnt_entry.get(), starter_entry.get())
 
-                driver.find_element_by_id("entrar").click()
-                time.sleep(1)
 
+start_btn.bind("<Button-1>", handle_click)
 
-
-                break
-            except:
-                pass
-
-
-
-
-
-def addUser(last_user):
-
-    global LIST_ADDED_USER
-    #Entrando na pagina para criar o user
-
-    driver.get(f"{LINK}/list_visitor")
-    time.sleep(1)
-    
-    #Criando novo usuario
-
-    btn_new = driver.find_element_by_id("btn_add_visitor").click()
-    time.sleep(2)
-
-
-    #Digitando o nome da usuario
-    name_input = driver.find_element_by_xpath("//div[@class='row form-group']//input[@id='name']")
-    name_input.send_keys(last_user)
-
-    #Digitando a data de expiração
-    driver.find_element_by_id("visitor_datelimit").clear()
-    date_picker = driver.find_element_by_id("visitor_datelimit")
-    date_picker.send_keys("18/01/2026")
-    time.sleep(1)
-
-    #Informaçoes adicionais (adicionar credito)
-
-
-    additional_info = driver.find_element_by_xpath("//li[@id='additional'][2]//a")
-    driver.execute_script("arguments[0].click();", additional_info)
-    add_info_btn = driver.find_element_by_xpath("//div[@class='portlet-title']//div[@class='actions']//a")
-    driver.execute_script("arguments[0].click();", add_info_btn)
-
-
-    time.sleep(1)
-    select = Select(driver.find_element_by_xpath("//div[@class='modal-content']//select"))
-    time.sleep(1)
-
-    select.select_by_value("number:1")
-    time.sleep(1)
-
-    driver.find_element_by_xpath("//div[@class='modal-content']//button[@id='btn_save']").click()
-
-
-
-    # Indo até a pagina de QR Code e gerando
-    qr_code = driver.find_element_by_xpath("//li[@id='additional']//a")
-    driver.execute_script("arguments[0].click();", qr_code)
-
-    generate_qr_code = driver.find_element_by_id("btn_create_qrcode")
-    driver.execute_script("arguments[0].click();", generate_qr_code)
-
-
-    print_qrcode = driver.find_element_by_id("btn_printqrcode")
-    driver.execute_script("arguments[0].click();", print_qrcode)
-
-
-
-
-    time.sleep(2)
-
-    #Salvando o usuario
-    save = driver.find_element_by_id("btn_save")
-    driver.execute_script("arguments[0].click();", save)
-
-    LIST_ADDED_USER.append(last_user)
-
-    time.sleep(1)
-
-
-
-
-def addUserContainer(times, start):
-   
-    for i in range(times):
-        try:
-            addUser(str(start + i))
-        except:
-            time.sleep(5)
-            addUser(str(start + i))
-        time.sleep(1)
-
-
-
-def main():
-    logIn()
-    t1 = time.time()
-    addUserContainer(qnt_docs, 1000)
-    t2 = time.time()
-    print(t2-t1)
-
-
-
-
-
-main()
-
-
+window.mainloop()
