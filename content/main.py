3c78a6b5d9ef824fc597d8277748eefa3ad15f29
pedriin1 <frigopedro@icloud.com>
2022-01-31 21:22:09 -0300

    addtional infos

--- content/main.py
+++ content/main.py
@@ -59,64 +59,6 @@ def logIn():
                 pass
 
 
-def addGroup():
-    driver.refresh()
-    driver.get(f"{LINK}/list_groups_visitors")
-
-    time.sleep(2)
-
-
-    driver.get(f"{LINK}/edit_group_visitors/{config['group_visitor_id']}")
-    time.sleep(1)
-
-    visitors_btn = driver.find_element_by_xpath("//li//a[@name='departiments_tab_users']")
-    driver.execute_script("arguments[0].click();", visitors_btn)
-
-    time.sleep(1)
-    driver.find_element_by_xpath("//div[@class='portlet-title']//div[@class='actions']//a").click()
-    time.sleep(1)
-
-    try:
-        driver.find_element_by_xpath("//thead/tr/th[6]").click()
-        time.sleep(2)
-        driver.find_element_by_xpath("//span[@id='select_all']//a").click()
-    except:
-        pass
-        
-   
-    add_btn = driver.find_element_by_xpath("//div[@class='modal-content']//div[@class='margiv-top-10']//button[@class='btn green ng-binding']")
-    driver.execute_script("arguments[0].click();", add_btn)
-
-    save_btn = driver.find_element_by_xpath("//div[@class='margiv-top-10']//button[@class='btn green ng-binding']")
-    driver.execute_script("arguments[0].click();", save_btn)
-
-
-
-def getLastUser():
-    driver.get(f"{LINK}/list_visitor")
-    time.sleep(1)
-
-    try:
-        get_firstuser = driver.find_element_by_xpath("//tbody[1]/tr[1]/td[2]")
-        get_firstuser = get_firstuser.text
-    except:
-        get_firstuser = 0
-   
-    select = Select(driver.find_element_by_name('datatablevisitor_length'))
-    select.select_by_value('10000')
-
-    
-    driver.find_element_by_xpath("//thead/tr/th[2]").click()
-    time.sleep(1)
-    get_lastuser = driver.find_element_by_xpath("//tbody[1]/tr[1]/td[2]")
-
-    try:
-
-        last_user_number = int(get_lastuser.text) + 1
-    except:
-        last_user_number = 1000
-
-    return last_user_number, int(get_firstuser or 0) 
 
 
 
@@ -144,7 +86,18 @@ def addUser(last_user):
     date_picker.send_keys("18/01/2026")
     time.sleep(1)
 
-    #Indo até a pagina de QR Code e gerando
+    #Informaçoes adicionais (adicionar credito)
+
+
+    additional_info = driver.find_element_by_xpath("//li[@id='additional'][2]//a")
+    driver.execute_script("arguments[0].click();", additional_info)
+
+
+
+    driver.find_element_by_xpath("//div[@class='portlet-title']//div[@class='actions']//a").click()
+
+
+    # Indo até a pagina de QR Code e gerando
     qr_code = driver.find_element_by_xpath("//li[@id='additional']//a")
     driver.execute_script("arguments[0].click();", qr_code)
 
@@ -161,8 +114,8 @@ def addUser(last_user):
     time.sleep(2)
 
     #Salvando o usuario
-    save = driver.find_element_by_id("btn_save")
-    driver.execute_script("arguments[0].click();", save)
+    # save = driver.find_element_by_id("btn_save")
+    # driver.execute_script("arguments[0].click();", save)
 
     LIST_ADDED_USER.append(last_user)
 
@@ -171,29 +124,14 @@ def addUser(last_user):
 
 
 
-def addUserContainer(times):
-    last_user, first_user = getLastUser()
-    times_arr = []
- 
-    if first_user != 1000:
-        for i in range(times):
-            if i+1000 < first_user:
-                times_arr.append(1000 + i)
-            else:
-                times_arr.append(i+last_user)
-
-    else:
-        for i in range(times):
-            times_arr.append(i+last_user)
-
-    print(last_user)
-    print(first_user)
-    for i in times_arr:
+def addUserContainer(times, start):
+   
+    for i in range(times):
         try:
-            addUser(str(i))
+            addUser(str(start + i))
         except:
             time.sleep(5)
-            addUser(str(i))
+            addUser(str(start + i))
         time.sleep(1)
 
 
@@ -201,8 +139,7 @@ def addUserContainer(times):
 def main():
     logIn()
     t1 = time.time()
-    addUserContainer(qnt_docs)
-    addGroup()
+    addUserContainer(qnt_docs, 1000)
     t2 = time.time()
     print(t2-t1)
 
---
+++ content/screen/interface.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+from tkinter import font
+
+
+
+window = tk.Tk()
+window.geometry("400x400")
+
+title_font = font.Font(family="Helvetica", size=20, weight=font.BOLD,
+                         )
+title = tk.Label(window, text="Criar usuário", 
+                         font=title_font, width="30", anchor="w", height="2")
+
+start_btn = tk.Button(window, text="Iniciar", width="40")
+entry = tk.Entry(window, width="40")
+entry.insert(0, 'username')
+title.pack(padx=10)
+entry.pack()
+start_btn.pack(padx=10)
+window.mainloop()
